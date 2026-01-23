02ff7434d4e3997315fe9b410cb5343dbe309b47
aserowy <serowy@hotmail.com>
2026-01-23 06:23:11 +0100

    collabora-online/collabora-online: init at 1.1.54
    Signed-off-by: aserowy <serowy@hotmail.com>

---
+++ charts/collabora-online/collabora-online/default.nix
@@ -0,0 +1,6 @@
+{
+  repo = "https://collaboraonline.github.io/online/";
+  chart = "collabora-online";
+  version = "1.1.54";
+  chartHash = "sha256-jDZMYEy8LGq1Rq9U61jWrfeP632RuYrPuEKAeuLMJoM=";
+}
