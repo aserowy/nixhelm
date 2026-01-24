9384242b11f6dd16b42e94368c86fc8281dfacf5
aserowy <serowy@hotmail.com>
2026-01-24 09:01:24 +0100

    haproxytech/kubernetes-ingress: init at 1.47.4
    Signed-off-by: aserowy <serowy@hotmail.com>

---
+++ charts/haproxytech/kubernetes-ingress/default.nix
@@ -0,0 +1,6 @@
+{
+  repo = "https://haproxytech.github.io/helm-charts";
+  chart = "kubernetes-ingress";
+  version = "1.47.4";
+  chartHash = "sha256-4TAJa/TsExH2aVaxgkcSnvOGW0K1/kSSO72cHqLJQjw=";
+}
